c4d57f35b4032b05f29d9d0f7fa37e20f3d7cd41
030831 <kdhc777@mail.ulsan.ac.kr>
2023-08-18 22:40:30 +0900

    Feat : 수상기록 등록/조회 컨트롤러 추가

--- src/main/java/com/gaonnuri/web/awards/controller/AwardsController.java
+++ src/main/java/com/gaonnuri/web/awards/controller/AwardsController.java
@@ -2,8 +2,10 @@ package com.gaonnuri.web.awards.controller;
 
 import com.gaonnuri.web.awards.repository.AwardsRepository;
 import com.gaonnuri.web.awards.service.AwardsService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,5 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class AwardsController {
     private final AwardsService awardsService;
+
+    @PostMapping("/add")
+    public String addAward() {
+
+        log.info("success addAward");
+        return "success addAward";
+    }
+
+    @GetMapping("/get")
+    public String getAward() {
+        log.info("success getAward");
+        return "success getAward";
+    }
 
 }
